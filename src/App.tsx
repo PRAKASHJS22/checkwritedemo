30d4fadbc50e4599b0b5c4b6e29c3f01b86d19dd
J, Prakash <prakash.j@legato.com>
2023-05-09 18:22:44 +0530

    dropdown added

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,11 @@
 // import Headers from "./components/headers/headers";
 import NmsProduction_Accounting from "./components/nms";
 
-// function App() {
-//   return (
-//     <div>
-//       <Headers />
-//       <NmsProduction_Accounting />
 
-//     </div>
-//   );
-// }
 
-// export default App;
-// import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
-// import Header from "./components/Header";
-import Headers from "./components/headers/headers";
+
+
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreateEmployee from "./components/CreateEmployee";
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Headers from "./headers/headers";
 import Footer from "./Footer";
+import NmsProduction_Accounting from "./nms";
+import { CustomDropDown } from "./customDropDown";
 
 const Layout = () => {
   return (
     <>
       <Headers />
-      <Footer/>
+      <NmsProduction_Accounting/>
+      <CustomDropDown/>
+      {/* <Footer/> */}
       <Outlet />
     </>
   );
